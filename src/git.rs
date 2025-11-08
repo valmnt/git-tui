7765393fe8d7d64f9288598ddee36763710d3800
valmnt <valentinmont8@gmail.com>
2025-11-08 11:15:08 +0100

    feat: fetch all branches

--- src/git.rs
+++ src/git.rs
@@ -1,9 +1,27 @@
-use git2::Repository;
+use git2::{self, BranchType, Repository};
 
-pub fn get_repo() {
-    let _repo = match Repository::open("./") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
-    println!("Opened repository at: {:?}", _repo.path());
+pub struct Git {
+    pub repo: Repository,
+}
+
+impl Git {
+    pub fn new() -> Self {
+        let repo = Repository::open("./").expect("failed to open repo");
+        Self { repo }
+    }
+
+    pub fn get_branches(&mut self) {
+        let _branches = match self.repo.branches(Some(BranchType::Local)) {
+            Ok(branches) => branches,
+            Err(e) => panic!("failed to open: {}", e),
+        };
+
+        for branch_result in _branches {
+            if let Ok((branch, kind)) = branch_result {
+                // branch.name() -> Result<Option<&str>, _>
+                let name = branch.name().ok().flatten().unwrap_or("<nom invalide>");
+                println!("{name} ({kind:?})");
+            }
+        }
+    }
 }
