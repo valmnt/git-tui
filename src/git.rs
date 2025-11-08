0c41d443687cfeac2f00bc60b5a1704b261b7fb4
valmnt <valentinmont8@gmail.com>
2025-11-08 14:30:24 +0100

    refactor: improve using rust

--- src/git.rs
+++ src/git.rs
@@ -13,34 +13,51 @@ impl Git {
     pub fn get_branches(&mut self) -> Vec<String> {
         let _branches = match self.repo.branches(Some(BranchType::Local)) {
             Ok(branches) => branches,
-            Err(e) => panic!("failed to open: {}", e),
+            Err(e) => panic!("failed to open: {e}"),
         };
 
         let mut names: Vec<String> = Vec::new();
 
         for branch_result in _branches {
             if let Ok((branch, _kind)) = branch_result {
-                let name = branch.name().ok().flatten().unwrap_or("invalid");
-                names.push(name.to_string());
+                let name = match branch.name() {
+                    Ok(Some(name)) => name,
+                    Ok(None) => "unknown",
+                    Err(e) => panic!("unable to get the branch name: {e}"),
+                }
+                .to_string();
+                names.push(name);
             }
         }
 
         return names;
     }
 
-    pub fn get_commits_branch(&mut self, branch: &str) {
-        let reference = format!("refs/heads/{}", branch);
-        let oid: git2::Oid = self.repo.revparse_single(&reference).unwrap().id();
+    pub fn get_commits_branch(&mut self, branch_name: &str) {
+        let branch = format!("refs/heads/{}", branch_name);
+        let oid: git2::Oid = match self.repo.revparse_single(&branch) {
+            Ok(obj) => obj.id(),
+            Err(e) => panic!("failed fetch oid: {e}"),
+        };
 
-        let mut walk = self.repo.revwalk().unwrap();
+        let mut walk = match self.repo.revwalk() {
+            Ok(walk) => walk,
+            Err(e) => panic!("failed to create walk: {e}"),
+        };
+
         let _ = walk.push(oid);
         let _ = walk.set_sorting(git2::Sort::TIME);
 
-        for oid in walk {
-            let commit = self.repo.find_commit(oid.unwrap());
-            let c = commit.unwrap();
-            let message = c.message().unwrap();
-            println!("{message}");
+        for result in walk {
+            if let Ok(oid) = result {
+                let commit = match self.repo.find_commit(oid) {
+                    Ok(commit) => commit,
+                    Err(e) => panic!("failed to find a commit: {e}"),
+                };
+
+                let message = commit.message().unwrap_or("no message");
+                println!("{message}");
+            }
         }
     }
 }
