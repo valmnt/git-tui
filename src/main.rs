b2668a1ad256e6c276cddb72e9e597999484cab5
valmnt <valentinmont8@gmail.com>
2025-11-09 12:52:52 +0100

    feat: add horizontal scroll

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use ratatui::{
 mod git;
 
 fn main() {
-    let app = App::new();
+    let mut app = App::new();
     let terminal = ratatui::init();
-    run(terminal, app);
+    run(terminal, &mut app);
 }
 
 struct App {
     tree_lines: Vec<String>,
+    view_width: usize,
     vertical_scroll: usize,
     horizontal_scroll: usize,
 }
@@ -25,19 +26,32 @@ impl App {
         let tree_lines = git.build_tree_lines();
         Self {
             tree_lines,
+            view_width: 0,
             vertical_scroll: 0,
             horizontal_scroll: 0,
         }
     }
 
-    fn scroll_up(&mut self, speed: usize) {
+    fn scroll_up_vertical(&mut self, speed: usize) {
         self.vertical_scroll = self.vertical_scroll.saturating_sub(speed)
     }
 
-    fn scroll_down(&mut self, speed: usize) {
+    fn scroll_down_vertical(&mut self, speed: usize) {
         let max_scroll = self.tree_lines.len().saturating_sub(speed);
         if self.vertical_scroll < max_scroll {
             self.vertical_scroll += speed;
+        }
+    }
+
+    fn scroll_up_horizontal(&mut self, speed: usize) {
+        self.horizontal_scroll = self.horizontal_scroll.saturating_sub(speed)
+    }
+
+    fn scroll_down_horizontal(&mut self, speed: usize) {
+        let longest = self.tree_lines.iter().map(|l| l.len()).max().unwrap_or(0);
+        let max_scroll = longest.saturating_sub(self.view_width);
+        if self.horizontal_scroll < max_scroll {
+            self.horizontal_scroll += speed;
         }
     }
 
@@ -49,17 +63,17 @@ impl App {
     }
 }
 
-fn run(mut terminal: DefaultTerminal, mut app: App) {
+fn run(mut terminal: DefaultTerminal, app: &mut App) {
     loop {
-        let _ = terminal.draw(|frame| draw(frame, &app));
-        handle_events(&mut app);
+        let _ = terminal.draw(|frame| draw(frame, app));
+        handle_events(app);
         if exit() {
             break;
         }
     }
 }
 
-fn draw(frame: &mut Frame, app: &App) {
+fn draw(frame: &mut Frame, app: &mut App) {
     let content = if app.tree_lines.is_empty() {
         "No git data available".to_string()
     } else {
@@ -67,6 +81,7 @@ fn draw(frame: &mut Frame, app: &App) {
     };
 
     let paragraph = Paragraph::new(content).scroll(app.scroll_offsets());
+    app.view_width = frame.area().width as usize;
     frame.render_widget(paragraph, frame.area());
 }
 
@@ -75,16 +90,22 @@ fn handle_events(app: &mut App) -> bool {
         if let Event::Key(key) = event {
             match key.code {
                 KeyCode::Char('h') => {
-                    app.scroll_up(15);
+                    app.scroll_up_vertical(15);
                 }
                 KeyCode::Char('j') => {
-                    app.scroll_down(15);
+                    app.scroll_down_vertical(15);
                 }
                 KeyCode::Char('k') => {
-                    app.scroll_up(5);
+                    app.scroll_up_vertical(5);
                 }
                 KeyCode::Char('l') => {
-                    app.scroll_down(5);
+                    app.scroll_down_vertical(5);
+                }
+                KeyCode::Char('o') => {
+                    app.scroll_up_horizontal(10);
+                }
+                KeyCode::Char('p') => {
+                    app.scroll_down_horizontal(10);
                 }
                 _ => {}
             }
